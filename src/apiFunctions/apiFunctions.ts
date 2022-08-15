67bd53ec1327e972e0b0a355f387486c56d93dd6
justintim3 <justlin90@gmail.com>
2022-08-15 15:30:14 -0700

    Delete src/apiFunctions directory
    Removed unused API functions

--- src/apiFunctions/apiFunctions.ts
+++
@@ -1,9 +0,0 @@
-import { apiFetchFactory } from "./apiFetchFactory";
-
-export const { getData, useGetData } = apiFetchFactory({
-  name: "getData",
-  url: "https://jsonplaceholder.typicode.com/todos/1",
-  options: {
-    method: "GET"
-  }
-});
